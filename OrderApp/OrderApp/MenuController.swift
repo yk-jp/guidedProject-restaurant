88e290ddb040b9b0d4c60cd870cf2e82480bb64d
yk-jp <k82.y12@gmail.com>
2022-05-21 11:07:30 -0700

    finish part4

---
+++ OrderApp/OrderApp/MenuController.swift
@@ -0,0 +1,83 @@
+//
+//  MenuController.swift
+//  OrderApp
+//
+//  Created by Yusuke K on 2022-05-21.
+//
+
+import Foundation
+
+typealias MinutesToPrepare = Int
+
+class MenuController {
+    let baseURL = URL(string: "http://localhost:8080/")!
+    
+    func fetchCategories() async throws -> [String] {
+        let categoriesURL = baseURL.appendingPathComponent("categories")
+        let (data, response) = try await URLSession.shared.data(from:categoriesURL)
+        
+        guard let httpResponse = response as? HTTPURLResponse,
+           httpResponse.statusCode == 200 else {
+            throw MenuControllerError.categoriesNotFound
+        }
+        
+        let decoder = JSONDecoder()
+        let categoriesResponse = try decoder.decode(CategoriesResponse.self,
+           from: data)
+        
+        return categoriesResponse.categories
+    }
+    
+    func fetchMenuItems(forCategory categoryName: String) async throws ->
+    [MenuItem] {
+        let baseMenuURL = baseURL.appendingPathComponent("menu")
+        var components = URLComponents(url: baseMenuURL, resolvingAgainstBaseURL: true)!
+        components.queryItems = [URLQueryItem(name: "category", value: categoryName)]
+        let menuURL = components.url!
+        
+        let (data, response) = try await URLSession.shared.data(from: menuURL)
+        
+        guard let httpResponse = response as? HTTPURLResponse,
+              httpResponse.statusCode == 200 else {
+            throw MenuControllerError.menuItemsNotFound
+        }
+        
+        let decoder = JSONDecoder()
+        let menuResponse = try decoder.decode(MenuResponse.self, from: data)
+        
+        return menuResponse.items
+        
+    }
+
+    func submitOrder(forMenuIDs menuIDs: [Int]) async throws -> MinutesToPrepare {
+        let orderURL = baseURL.appendingPathComponent("order")
+        var request = URLRequest(url: orderURL)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let menuIdsDict = ["menuIds": menuIDs]
+        let jsonEncoder = JSONEncoder()
+        let jsonData = try? jsonEncoder.encode(menuIdsDict)
+        
+        request.httpBody = jsonData
+        
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        guard let httpResponse = response as? HTTPURLResponse,
+           httpResponse.statusCode == 200 else {
+            throw MenuControllerError.orderRequestFailed
+        }
+        
+        let decoder = JSONDecoder()
+        let orderResponse = try decoder.decode(OrderResponse.self, from: data)
+        
+        return orderResponse.prepTime
+    }
+    
+    enum MenuControllerError: Error, LocalizedError {
+        case categoriesNotFound
+        case menuItemsNotFound
+        case orderRequestFailed
+    }
+}
+
