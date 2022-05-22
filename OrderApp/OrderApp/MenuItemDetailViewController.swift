c7846fa19f46ab2ea76e25ca7c089fb764bbd77b
yk-jp <k82.y12@gmail.com>
2022-05-22 10:39:18 -0700

    finish part7

--- OrderApp/OrderApp/MenuItemDetailViewController.swift
+++ OrderApp/OrderApp/MenuItemDetailViewController.swift
@@ -11,6 +11,12 @@ import UIKit
 class MenuItemDetailViewController: UIViewController {
     let menuItem: MenuItem
     
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var priceLabel: UILabel!
+    @IBOutlet var detailTextLabel: UILabel!
+    @IBOutlet var addToOrderButton: UIButton!
+    
     init?(coder: NSCoder, menuItem: MenuItem) {
         self.menuItem = menuItem
         super.init(coder: coder)
@@ -22,19 +28,23 @@ class MenuItemDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        updateUI()
+    }
+ 
+    @IBAction func orderButtonTapped(_ sender: UIButton) {
+        UIView.animate(withDuration: 0.5, delay: 0,
+               usingSpringWithDamping: 0.7, initialSpringVelocity: 0.1,
+               options: [], animations: {
+                self.addToOrderButton.transform =
+                   CGAffineTransform(scaleX: 2.0, y: 2.0)
+                self.addToOrderButton.transform =
+                   CGAffineTransform(scaleX: 1.0, y: 1.0)
+            }, completion: nil)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func updateUI() {
+        nameLabel.text = menuItem.name
+        priceLabel.text = menuItem.price.formatted(.currency(code: "usd"))
+        detailTextLabel.text = menuItem.detailText
     }
-    */
-
 }
